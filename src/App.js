1f59c3428a7aa9986cacb7214a64c6334d92b6b8
akiyoshi <yuiko.akiyoshi@nijibox.co.jp>
2019-09-15 17:08:06 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div>
-      <h1>hello workd</h1>
-    </div>
-  );
+  // const greeting ="hi!";
+  // const dom = <div className = "foo"> {greeting} </div>
+  return(
+  <React.Fragment>
+    <label htmlFor="bar">bar</label>
+    <input type="text" onChange={() =>{console.log("I am clicked")}}/>
+  </React.Fragment>
+  )
 }
 
 export default App;
