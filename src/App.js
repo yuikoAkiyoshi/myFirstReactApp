9dc2b6e5f25f2c2d0c74b6b4f07d8b79d2e5a9c5
akiyoshi <yuiko.akiyoshi@nijibox.co.jp>
2019-09-15 17:40:47 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
 
 function App() {
-  // const greeting ="hi!";
-  // const dom = <div className = "foo"> {greeting} </div>
+  const profiles = [
+    {name:"Taro",age:10},
+    {name:"Hanako",age:5},
+    {name:"hoge"}
+  ]
   return(
   <React.Fragment>
+    {profiles.map((profiles, index)=>{
+      return <User name={profiles.name} age={profiles.age} key={index}/>
+    })
+    }
     <label htmlFor="bar">bar</label>
     <input type="text" onChange={() =>{console.log("I am clicked")}}/>
-    <Cat />
+    <User name={"Taro"} age={10}/>
+    <User name={"Hanako"} age={5}/>
   </React.Fragment>
   )
 }
 
-const Cat =() => {
-  return<div>meow</div>
+const User =(props) => {
+  return<div>hi, i am {props.name} and I am {props.age}years old</div>
+}
+
+User.defaultProps ={
+  age:1
 }
 
 export default App;
