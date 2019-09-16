ece878664d528a7a4876acdd4f1c325fb3a216c0
akiyoshi <yuiko.akiyoshi@nijibox.co.jp>
2019-09-16 11:13:39 +0900

    fix typo

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
-import reducer from './reducers';
-import App from './compornent/App';
+import reducer from './redusers';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 
-const store = createStore(reduce)
+const store = createStore(reducer)
 
 ReactDOM.render(
     <Provider store ={store}>
--- src/redusers/count.js
+++ src/redusers/count.js
@@ -8,7 +8,7 @@ export default (state = initialState, action) => {
             return{ value: state.value +1 }
         case DECREMENT:
             return{ value: state.value - 1 }
-        default
+        default:
             return state
     }
 }
