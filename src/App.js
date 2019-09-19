806c041c199b5aff8067bf1f6d8af6a5d2f05a29
akiyoshi <yuiko.akiyoshi@nijibox.co.jp>
2019-09-19 17:30:46 +0900

    make app day1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import title from './img/title.png';
+import kabakichi from './img/kabakiti_yoko.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello workd
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//関数コンポーネント
+const App =() => (<Counter></Counter>)
+
+  class Counter extends Component{
+    constructor(){
+      super()
+      this.state ={
+        age:26,
+        income:25,
+        savings:700,
+        old:0.7
+      }
+    }
+    // inputテキストの動的入れ替え処理
+    onChangeText1(event) {
+      const value = event.target.value;
+      this.setState({
+        age: value,
+      });
+    }
+    onChangeText2(event) {
+      const value = event.target.value;
+      this.setState({
+        income: value,
+      });
+    }
+    onChangeText3(event) {
+      const value = event.target.value;
+      this.setState({
+        savings: value,
+      });
+    }
+    onChangeText4(event) {
+      const value = event.target.value;
+      this.setState({
+        old: value,
+      });
+    }
+    //button押した時に結果欄を表示
+    onClickShow() {
+      console.log(this.state.age)
+      console.log(this.state.income)
+      console.log(this.state.savings)
+      console.log(this.state.old)
+    }
+
+    render(){
+      return (
+        <React.Fragment>
+          <div className="mainWrapper">
+            <div className="subWrapper">
+              <div className="title"><img src={title} alt="必要貯蓄額シュミュレーター"></img></div>
+              <div className="wrapper">
+                <div className ="inputArea">
+                  <div className="inputArea__container">
+                    <p className="inputArea__text">▼入力してください</p>
+                    <div className="inputArea__wrapper">
+                      <label className="inputItem">
+                        <span className="inputTitle">年齢</span>
+                        <input type="number" onChange={event => this.onChangeText1(event)}/>
+                      </label>
+                      <label className="inputItem">
+                        <span className="inputTitle">手取り月収（万）</span>
+                        <input type="number" onChange={event => this.onChangeText2(event)} />
+                      </label>
+                      <label className="inputItem">
+                        <span className="inputTitle">現在貯蓄額（万）</span>
+                        <input type="number" onChange={event => this.onChangeText3(event)} />
+                      </label>
+                      <label className="inputItem">
+                        <span className="inputTitle">老後生活水準</span>
+                        <input type="number" step="0.1" onChange={event => this.onChangeText4(event)} />
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <div className="simulation">
+                  <div className="simulation__container">
+                    <button　className="simulation__btn" onClick={event => this.onClickShow()}>シュミレーション開始</button>
+                  </div>
+                </div>
+                <div className="resultArea">
+                  <div className="resultArea__container">
+                    <div className="resultArea__text">あなたの月間必要貯蓄額は</div>
+                    <Amount amount={(Math.floor(((this.state.old*this.state.income*12 -(this.state.income)*12*0.3-(this.state.savings-1000)/30)/(((65 - this.state.age)/30+this.state.old)*this.state.income*12)*this.state.income)*10))/10} />
+                    <div className="resultArea__text">万円です！</div>
+                  </div>
+                </div>
+                <div className="kabakichi">
+                  <div class='arrow_box'>がんばって貯金しよう！</div>
+                  <img src={kabakichi} alt='かばきち'></img>
+                </div>
+              </div>
+            </div>
+          </div>
+        </React.Fragment>
+      );
+    }
+}
+
+//動的に変更される必要貯蓄額
+
+const Amount =(props) =>{
+  return<div className="resultArea__amount">{props.amount}</div>
 }
 
 export default App;
